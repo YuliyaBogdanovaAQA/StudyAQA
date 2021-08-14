d2f98acce48c6fbe953b2f2a498009784dfd549e
YuliyaBogdanova <juliete.b25@gmail.com>
2021-08-14 23:27:49 +0300

    another solution
    There are solution for checking an array whith a size that will be set by the user.

--- StudyAQA/src/main/java/shcool/lesson6/HomeWorkSix.java
+++ StudyAQA/src/main/java/shcool/lesson6/HomeWorkSix.java
@@ -1,14 +1,14 @@
 package shcool.lesson6;
 
 class MyArraySizeException extends Exception {
-    public MyArraySizeException(int sizeArray) {
-        super("The size of array not: " + sizeArray);
+    public MyArraySizeException(String message) {
+        super(message);
     }
 }
 
 class MyArrayDataException extends Exception {
-    public MyArrayDataException(int row, int column) {
-        super("Incorrect data in point number: " + row + "/" + column);
+    public MyArrayDataException(String message) {
+        super(message);
     }
 }
 
@@ -17,21 +17,22 @@ public class HomeWorkSix {
         String[][] myArray = new String[4][7];
         String[][] myArray2 = new String[4][4];
 
-        try {
+      /*  try {
             TaskTwo(myArray,4,7);
         } catch (MyArraySizeException e) {
             e.printStackTrace();
         } catch (MyArrayDataException e) {
             e.printStackTrace();
-        }
+        }*/
 
         String[][] forCheck = {{"1", "1", "1", "1"},
                 {"1", "1", "kdngkj", "1"},
-                {"1", "1", "1", "1"},
-                {"1", "1", "1", "jhv"}};
+                {"jgjb", "1", "1", "1"},
+                {"1", "1", "1", "jhv"}
+                };
 
         try {
-            TaskTwo(forCheck, 4, 4);
+            TaskTwo(forCheck, 4, 3);
         } catch (MyArraySizeException e) {
             e.printStackTrace();
         } catch (MyArrayDataException e) {
@@ -42,17 +43,19 @@ public class HomeWorkSix {
     public static void TaskTwo(String[][] array, int sizeOfRow, int sizeOfColumn) throws MyArraySizeException, MyArrayDataException {
         int sumElements = 0;
         if (array.length != sizeOfRow) {
-            throw new MyArraySizeException(sizeOfRow);
+            throw new MyArraySizeException("The number of rows not: " +sizeOfRow);
         }
         for (int i = 0; i < array.length; i++) {
             if (array[i].length != sizeOfColumn) {
-                throw new MyArraySizeException(sizeOfColumn);
+                throw new MyArraySizeException("The number of columns not: "+ sizeOfColumn);
             }
             for (int j = 0; j < array[i].length; j++) {
                 try {
                     sumElements += Integer.parseInt(array[i][j]);
                 } catch (NumberFormatException e) {
-                    throw new MyArrayDataException(i, j);
+                    Throwable x = new MyArrayDataException("Incorrect data in point with index: " +
+                            i +"/" + j + " " + e.getMessage());
+                    x.printStackTrace();
                 }
             }
         }
